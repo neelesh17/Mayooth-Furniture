b2607804b29c06812201b2e3b0ce80269c48de16
Neelesh Thakur <nt00605@gmail.com>
2020-07-14 11:17:01 +0530

    added fonts and completed navbar

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
-import {ReactComponent as Logo} from '../../assets/Logo-Maynooth-Option1.svg';
+import {ReactComponent as Logo} from '../../assets/Logo-Maynooth-Option3.svg';
 
-import {  NavContainer, LogoContainer } from './nav.styles';
+import {  
+    NavContainer, LogoContainer, InputContainer,
+    UserIconContainer, CartIconContainer, ButtonContainer, Button 
+} from './nav.styles';
 
 const Navbar = () => (
     <NavContainer>
         <LogoContainer to="/">
             <Logo classname="logo" />
         </LogoContainer>
+        <InputContainer type="search" placeholder=" Search"/>
+        <UserIconContainer>
+            <i class="ri-user-add-line"></i>
+        </UserIconContainer>
+        <CartIconContainer>
+            <i class="ri-shopping-cart-line"></i>
+        </CartIconContainer>
+        <ButtonContainer>
+            <Button >Living Room<Link to="/" /></Button>
+            <Button >Kitchen&Dinner</Button>
+            <Button >Bedroom</Button>
+            <Button >Contact Us</Button>
+        </ButtonContainer>
     </NavContainer>
 );
 
--- src/components/nav/nav.styles.jsx
+++ src/components/nav/nav.styles.jsx
@@ -7,11 +7,60 @@ export const NavContainer = styled.div`
     display: flex;
     justify-content: space-between;
     background-color: rgba(0,0,0,0.7);
-    
 `;
 
 export const LogoContainer = styled(Link)`
-    width: 226.62px;
+    position: relative;
+    padding-top: 0.625em;
+    padding-left: 17.25em;
+    width: 14.1875em;
     height: 100%;
-    padding: 25px;
 `;
+
+export const UserIconContainer = styled.div`
+    position: absolute;
+    top: 10px;
+    left: 1544px;
+    font-size: 43px;
+    color: white;
+`;
+export const CartIconContainer = styled.div`
+    position: absolute;
+    top: 14px;
+    left: 1600px;
+    font-size: 35px;
+    color: white;
+`;
+
+ export const InputContainer = styled.input`
+    position: absolute;
+    top: 13px;
+    left: 581px;
+    width: 866px;
+    height: 38px;
+    border: None;
+    border-radius: 20px;
+ `;
+
+ export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    top: 62px;
+    left: 280px;
+    width: 1373px;
+    height: 40px;
+ `;
+
+ export const Button = styled(Link)`
+    width: 133px;
+    height: 40px;
+    border-radius: 20px;
+    text-decoration: none;
+    text-align: center;
+    line-height: 40px;
+    font: Regular 16px Open Sans;
+    color: rgba(39, 149, 76, 1);
+    background-color: rgba(255, 255,255, 1);
+    box-shadow:  0 1px 3px rgba(0, 0, 0, 0.2);
+ `;

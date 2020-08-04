74b32cca8ac809d606aa8f5c0d1f2ef51d99c8e0
Neelesh Thakur <nt00605@gmail.com>
2020-08-04 13:09:12 +0530

    changed for deploymen

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(require("express-session")({
   resave: false,
   saveUninitialized: false
 }));
-app.use(passport.initialize());
+app.use(passport.initialize()) ;
 app.use(passport.session());
 
 passport.use(new LocalStrategy(User.authenticate()));
